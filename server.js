491e2081f5db3049c7b6b52b66c9d17649dc1cb7
Cristian Cristancho <Cristian.Cristancho@endava.com>
2019-02-22 20:01:37 -0500

    Change server again

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('<h1>Hello mauricio world planet</h1>\n');
+    res.send('<h1>Hello cccc world planet</h1>\n');
 });
 
 app.listen(PORT, HOST);
